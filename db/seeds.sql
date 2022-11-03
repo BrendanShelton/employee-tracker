96b8e8f8343802a4cddbdd509a596a1b7ba353fe
Brendan Shelton <brendan.d.shelton@gmail.com>
2022-11-03 14:22:23 -0400

    worked on index.js and seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,11 +1,15 @@
 INSERT INTO department (name)
-VALUES (),
-       ();
+VALUES ("department1"),
+       ("department2");
 
 INSERT INTO role (title, salary, department_id)
-VALUES (),
-       ();
+VALUES  ("manager", 100000, 1),
+        ("manager", 100000, 2),
+        ("worker", 50000, 1),
+        ("worker", 50000, 2);
 
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES (),
-       ();
+/*INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES  ("firstname1", "lastname1", 1, NULL),
+        ("firstname2", "lastname2", 2, 1),
+        ("firstname3", "lastname3", 3, NULL),
+        ("firstname4", "lastname4", 4, 2);*/
--- index.js
+++ index.js
@@ -1,70 +1,104 @@
 const inquirer = require('inquirer');
 const cTable = require('console.table');
+const mysql = require('mysql2');
+
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    // MySQL username,
+    user: 'root',
+    // MySQL password
+    password: 'root',
+    database: 'employees_db'
+  },
+  console.log(`Connected to the books_db database.`)
+);
+
+const employees = []
+/*db.query('SELECT * FROM employee', function (err, results) {
+  console.log(results);
+});*/
 
 const questions = [{
     type: 'list',
     message: 'Select an option',
     name: 'option',
-    choices: ['Engineer', 'Intern']
+    choices: ['view all departments', 'view all roles', 'view all employees', 
+      'add a department', 'add a role', 'add an employee', 
+      'update an employee role']
   },
   {
     type: 'input',
-    message: 'Enter the team manager\'s employee ID',
-    name: 'ID',
+    message: 'What is the name of the department you are adding?',
+    name: 'department',
+    when: (answers) => answers.option === "add a department"
   },
   {
     type: 'input',
-    message: 'Enter the team manager\'s email address',
-    name: 'email',
+    message: 'What is the name of the role you are adding?',
+    name: 'role',
+    when: (answers) => answers.option === "add a role"
   },
   {
     type: 'input',
-    message: 'Enter the team manager\'s office number',
-    name: 'office',
+    message: 'What is the salary of the role you are adding?',
+    name: 'salary',
+    when: (answers) => answers.option === "add a role"
   },
   {
-    type: 'loop',
-    message: 'Do you want to add more team members?',
-    name: 'members',
-    questions: [
-        {
-            type: 'list',
-            message: 'What is the next team member\'s role',
-            name: 'role',
-            choices: ['Engineer', 'Intern']
-        },
-        {
-            type: 'input',
-            message: 'Enter the team member\'s name',
-            name: 'name',
-        },
-        {
-            type: 'input',
-            message: 'Enter the team member\'s employee ID',
-            name: 'ID',
-        },
-        {
-            type: 'input',
-            message: 'Enter the team member\'s email address',
-            name: 'email',
-        },
-        {
-            type: 'input',
-            message: 'What is the team member\'s github username?',
-            name: 'github',
-            when: (answers) => answers.role === "Engineer"
-        },
-        {
-            type: 'input',
-            message: 'What is the name of the team member\'s school?',
-            name: 'school',
-            when: (answers) => answers.role === "Intern"
-        },
-    ]
+    type: 'input',
+    message: 'What department is the role you are adding part of?',
+    name: 'roleDepartment',
+    when: (answers) => answers.option === "add a role"
+  },
+  {
+    type: 'input',
+    message: 'What is the employee\'s first name?',
+    name: 'firstName',
+    when: (answers) => answers.option === "add an employee"
+  },
+  {
+    type: 'input',
+    message: 'What is the employee\'s last name?',
+    name: 'lastName',
+    when: (answers) => answers.option === "add an employee"
+  },
+  {
+    type: 'input',
+    message: 'What is the employee\'s role?',
+    name: 'employeeRole',
+    when: (answers) => answers.option === "add an employee"
+  },
+  {
+    type: 'input',
+    message: 'What is the ID of the employee\'s manager?',
+    name: 'employeeManager',
+    when: (answers) => answers.option === "add an employee"
+  },
+  {
+    type: 'list',
+    message: 'Which employee\'s role do you want to update?',
+    name: 'UpdatedEmployee',
+    choices: employees,
+    when: (answers) => answers.option === "update an employee role"
   }];
 
 function init() {
     inquirer
     .prompt(questions)
-    .then((response) => getTeamMembers(response))
+    .then((response) => handleResponse(response))
   }
+
+function handleResponse(response) {
+  console.log(response)
+  switch(response.option) {
+    case "view all departments":
+      console.log("department chosen")
+      break;
+    case "":
+
+    case "":
+  }
+}
+
+init()
