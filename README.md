1ff6e1024db2d52c031767d05c65ac30eaea1c5d
Brendan Shelton <brendan.d.shelton@gmail.com>
2022-11-09 08:30:30 -0500

    finished comments and README

--- README.md
+++ README.md
@@ -1,1 +1,45 @@
 # employee-tracker
+
+## Description
+
+This application allows the user to view the "department", "role", and "employee" tables, add entries to those tables, and to update the roles of employees
+
+## Installation
+
+This application requires node.js and MySQL Server. Open the main folder containing "index.js" of the application in a terminal and enter "npm install".
+
+## Usage
+
+Open a MySQL shell in the "db" folder and enter "source schema.sql" create the "employees_db" database. This will also drop any existing database with that name. To seed the database with sample data, enter "source seeds.sql". Open the main folder containing "index.js" in a terminal and enter "node index" and then select whether to view a table, add to a table, or update an entry. Enter any data the prompt asks for. The application may need to be exited and restarted in order to view newly entered data. 
+
+https://github.com/BrendanShelton/employee-tracker
+
+![screenshot of application](screenshot.PNG)
+
+## Credits
+
+N/A
+
+## License
+
+MIT License
+
+Copyright (c) 2022 BrendanShelton
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const inquirer = require('inquirer');
 const cTable = require('console.table');
 const mysql = require('mysql2');
 
-
+//creates connection to database
 const db = mysql.createConnection(
   {
     host: 'localhost',
@@ -17,19 +17,20 @@ const db = mysql.createConnection(
 
 let questions
 
-
+//gets an array of employees which will be used in the question choices
 db.query('SELECT * FROM employee', function (err, results) {
 
   let employees = results.map(employeeObj => {
     return employeeObj.first_name + " " + employeeObj.last_name
   })
-  
+//gets an array of roles which will be used in the question choices
   db.query('SELECT * FROM role', function (err, results) {
   
     let roles = results.map(roleObj => {
       return roleObj.title;
     })
-    
+
+//the questions the user will be asked
     questions = [{
       type: 'list',
       message: 'Select an option',
@@ -75,9 +76,10 @@ db.query('SELECT * FROM employee', function (err, results) {
       when: (answers) => answers.option === "add an employee"
     },
     {
-      type: 'input',
+      type: 'list',
       message: 'What is the employee\'s role?',
       name: 'employeeRole',
+      choices: roles,
       when: (answers) => answers.option === "add an employee"
     },
     {
@@ -96,166 +98,171 @@ db.query('SELECT * FROM employee', function (err, results) {
     {
       type: 'list',
       message: 'What is the employee\'s new role?',
-      name: 'pdatedEmployeeRole',
+      name: 'updatedEmployeeRole',
       choices: roles,
       when: (answers) => answers.option === "update an employee role"
     }];
   
-    init()
 
+    init()
   })
-  
   
 });
 
-
-  
+//displays the questions for the user
 function init() {
     inquirer
     .prompt(questions)
     .then((response) => handleResponse(response))
   }
 
+//called after the user answers questions
 function handleResponse(response) {
-  console.log(response)
   switch(response.option) {
     case "view all departments":
       viewDepartments(response)
+      init()
       break;
 
     case "view all roles":
       viewRoles(response);
-
+      init()
       break;
       
     case "view all employees":
       viewEmployees(response);
+      init()
       break;
 
     case "add a department":
       addDepartment(response)
-
+      init()
       break;
 
     case "add a role":
       addRole(response)
-
+      init()
       break;
 
     case "add an employee":
       addEmployee(response)
-          
+      init()
       break;
 
     case "update an employee role":
       updateRole(response)
-
+      init()
       break;
 
+    case "quit":
+      process.exit();
+
+      break;
     default:
       console.log("no option chosen")
   }
 }
 
 function viewDepartments(response) {
-  //console.log("department chosen")
+//selects and displays the "department" table 
   db.query('SELECT * FROM department', function (err, results) {
+    console.log("\b");
     console.table(results);
   })
-  //init()
+ 
 }
 
 function viewRoles(response) {
-  //console.log("")
+//selects and displays the "role" table with the corresponding department joined to each role
   db.query('SELECT * FROM role JOIN department ON role.department_id = department.id', function (err, results) {
+    console.log("\b");
     console.table(results);
   })
 }
 
 function viewEmployees(response) {
-  //console.log("")
-  db.query('SELECT employee.id, first_name, last_name FROM employee JOIN role ON employee.role_id = role.id', function (err, results) {
+//selects and displays the "employee" table with the corresponding role joined to each employee
+  db.query('SELECT * FROM employee JOIN role ON employee.role_id = role.id', function (err, results) {
+    console.log("\b");
     console.table(results);
   })
 }
 
 function addDepartment(response) {
-  console.log("add a department chosen")
-      db.query(`INSERT INTO department(name) VALUES (?)`, response.department, function (err, results) {
-        if (err) {
-          console.log(err);
-        }
-        console.log(`Added ${response.department} to database.`);
-      })
+//inserts the department the user entered into the "department" table
+  db.query(`INSERT INTO department(name) VALUES (?)`, response.department, function (err, results) {
+    if (err) {
+      console.log(err);
+    }
+      console.log("\b");
+      console.log(`Added ${response.department} to database.`);
+    })
 }
 
 function addRole(response) {
-  console.log("add a role chosen")
+
   let id
+//gets the id of the department the user entered
   db.query('SELECT * FROM department', function (err, results) {
     if (err) {
       console.log(err);
-  }
-    //console.log(results);
-    //console.log("first row:");
-    //console.log(results[0]);
+    }
     for(row of results) {
       console.log(row)
       if (row.name === response.roleDepartment) {
         id = row.id
       }
+      
   }
+//inserts the role the user entered into the "role" table if user entered a valid department
+  if (!id) {
+    console.error("department does not exist")
+  } else {
     db.query(`INSERT INTO role(title, salary, department_id) VALUES (?,?,?)`, [response.role, response.salary, id], function (err, results) {
       if (err) {
         console.log(err);
       }
+      console.log("\b");
       console.log(`Added ${response.role} to database.`);
   })
+  }
+
 })
 }
 
 function addEmployee(response) {
-  console.log("add an employee chosen")
   let roleId
   let manager = response.employeeManager.split(" ")
   let managerId
+
+//gets the id of the role the user entered
   db.query('SELECT * FROM role', function (err, results) {
     if (err) {
       console.log(err);
     }
     
     for(row of results) {
-      console.log(row)
       if (row.title === response.employeeRole) {
         roleId = row.id
       }
     }
-    db.query('SELECT * FROM employee', function (err, results) {
+//inserts the employee the user entered into the "role" table if user entered a valid manager
+    db.query(`INSERT INTO employee(first_name, last_name, role_id, manager_id) VALUES (?,?,?,?)`, [response.firstName, response.lastName, roleId, response.employeeManager], function (err, results) {
       if (err) {
-        console.log(err);
+      console.log(err);
       }
-      for(row of results) {
-        console.log(row)
-        if (row.first_name === manager[0] && row.last_name === manager[1]) {
-          managerId = row.id
-        }
-      }
-        db.query(`INSERT INTO employee(first_name, last_name, role_id, manager_id) VALUES (?,?,?,?)`, [response.firstName, response.lastName, roleId, managerId], function (err, results) {
-          if (err) {
-          console.log(err);
-          }
-            console.log(`Added ${response.firstName} ${response.lastName} to database.`);
-          })
-        })
+      console.log("\b");
+      console.log(`Added ${response.firstName} ${response.lastName} to database.`);
+      })
+   
   })
 }
 
 function updateRole(response) {
-  console.log("updating role")
-  console.log(response)
   let roleId
+//splits the first and last name of the employee the user chose
   employeeArr = response.updatedEmployee.split(' ')
-
+//gets the role the user entered
   db.query('SELECT * FROM role', function (err, results) {
   
     for(row of results) {
@@ -263,10 +270,12 @@ function updateRole(response) {
         roleId = row.id
       }
   }
+//updates the role of the chosen employee to the role the chosen role
+    db.query('UPDATE employee WHERE first_name = ? and last_name = ? SET role_id = ?', [employeeArr[0], employeeArr[1], roleId], function (err, results) {
+      console.log("\b");
+      console.log(`updated role of ${response.updatedEmployee} to ${response.updatedEmployeeRole}`);
+    })
   })
 
-
-  db.query('UPDATE employee WHERE first_name = ? and last_name = ? SET role_id = ?', [employeeArr[0], employeeArr[1], roleId], function (err, results) {
-    console.log(`updated role of ${response.updatedEmployee} to ${response.updatedEmployeeRole}`);
-  })
+  
 }
